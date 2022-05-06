c3b6ade4d920bff334872f02f09723d03b4c3cc7
MariaGoemans1 <99274593+MariaGoemans@users.noreply.github.com>
2022-05-06 19:25:57 +0000

    Added spacing between sections in order.php to improve readability

--- order.php
+++ order.php
@@ -1,17 +1,23 @@
 <?php
   //setting a constant for tax
   define("HST", 0.13);
-  //initializing variables
+
+  //initializing variable for the base sandwich cost
   $baseSandCost = 0;
+
   //declaring variables for base sandwich and sides
   $meatType = $_POST["meat-type"];
   $sandSize = $_POST["sand-size"];
   $numDrinks = intval($_POST["drinks"]);
   $numPotatoes = intval($_POST["potatoes"]);
-  //Changing the number of toppings based on the number of checkboxes checked - method found at https://stackoverflow.com/questions/18572351/counting-how-many-checkbox-are-checked-php-html
+
+  //initializing variable for the number of toppings
   $numToppings = 0;
+
+  //Changing the number of toppings based on the number of checkboxes checked - method found at https://stackoverflow.com/questions/18572351/counting-how-many-checkbox-are-checked-php-html
   //if tabbouleh is checked
   if(isset($_POST["tabbouleh"])){
+    //++ adds one to the total
     $numToppings++;
   }
   //if hummus is checked
@@ -34,8 +40,10 @@
   if(isset($_POST["onions"])){
     $numToppings++;
   }
+
   //calculating the total cost of toppings
   $costToppings = $numToppings * 0.25;
+
   //determining the cost of the base sandwich using compound if/then statements
   //if the meat type is chicken and the size is small
   if ((meatType == "chicken") && (sandSize == "small")) {
@@ -61,13 +69,16 @@
   else {
     $baseSandCost = 8.50;
   }
+
   //determining the cost of the sides
   $costDrinks = $numDrinks * 3.00;
   $costPotatoes = $numPotatoes * 6.00;
-  //calculating the subtotal
+
+  //calculating the totals
   $subtotal = $baseSandCost + $costToppings + $costDrinks + $costPotatoes;
   $tax = $subtotal * HST;
   $total = $subtotal + $tax;
-  //displaying the cost to the screen
+
+  //displaying the cost (totals) to the screen
   echo "Your subtotal is $" . number_format($subtotal, 2) . ".<br>The amount of HST added is $" . number_format($tax, 2) . ".<br>Your total is $" . number_format($total, 2) . ".";
 ?>
