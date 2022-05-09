9b6c36f2251a332098518c86c3e59bdd9c5da578
MariaGoemans1 <99274593+MariaGoemans@users.noreply.github.com>
2022-05-09 22:03:55 +0000

    Began styling in css

--- index.php
+++ index.php
@@ -12,42 +12,56 @@
     <link rel="icon" type="image/png" sizes="32x32" href="./favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="./favicon/favicon-16x16.png">
     <link rel="manifest" href="./favicon/site.webmanifest">
+    <!-- Code for MDL -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.cyan-red.min.css">
     <!-- Link to CSS file -->
     <link rel="stylesheet" href="./css/style.css">
     <!-- Title -->
     <title>Shawarma Haven Menu - Maria Goemans</title>
   </head>
   <body>
-    <?php echo "<h1>Shawarma Haven - PHP Menu</h1>"; ?>
+    <!-- More MDL code -->
+    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
     <?php
+      echo "<h2>Shawarma Haven - PHP Menu</h2>";
       echo "<p>Welcome to Shawarma Haven! Use this web page to view our menu and place your order online! Then, review your order and total and submit it to us. You can get your order delivered or you can select a location for pickup.</p>";
-      echo "<h3>Our Menu and Prices:</h3>";
     ?>
-    <!-- Table listing options and prices -->
-    <table>
-      <tr>
-        <td>Chicken Shawarma Sandwich</td>
-        <td>Small: $6.00<br>Medium: $7.00<br>Large: $8.00</td>
-      </tr>
-      <tr>
-        <td>Beef Shawarma Sandwich</td>
-        <td>Small: $6.50<br>Medium: $7.50<br>Large: $8.50</td>
-      </tr>
-      <tr>
-        <td>Toppings (tabbouleh, hummus, pickles, onions, etc.)</td>
-        <td>$0.25 each</td>
-      </tr>
-      <tr>
-        <td>Drinks (canned and bottled juices and pops)</td>
-        <td>$3.00 each</td>
-      </tr>
-      <tr>
-        <td>Garlic Potatoes</td>
-        <td>$6.00</td>
-      </tr>
-    </table>
-    <h3>Your Order:</h3>
-    <h4>Choose Your Base Shawarma Sandwich:</h4>
+    <!-- Div for style -- background colour -->
+    <div id="first-coloured-div">
+      <?php echo "<h4>Our Menu and Prices:</h4>"; ?>
+      <center>
+        <!-- Table listing options and prices -->
+        <table>
+          <tr>
+            <th>Item</th>
+            <th>Price</th>
+          </tr>
+          <tr>
+            <td>Chicken Shawarma Sandwich</td>
+            <td>Small: $6.00, Medium: $7.00, Large: $8.00</td>
+          </tr>
+          <tr>
+            <td>Beef Shawarma Sandwich</td>
+            <td>Small: $6.50, Medium: $7.50, Large: $8.50</td>
+          </tr>
+          <tr>
+            <td>Toppings (tabbouleh, hummus, pickles, onions, etc.)</td>
+            <td>$0.25 each</td>
+          </tr>
+          <tr>
+            <td>Drinks (canned and bottled juices and pops)</td>
+            <td>$3.00 each</td>
+          </tr>
+          <tr>
+            <td>Garlic Potatoes</td>
+            <td>$6.00</td>
+          </tr>
+        </table>
+      </center>
+    </div>
+    <h4>Your Order:</h4>
+    <h5>Choose Your Base Shawarma Sandwich:</h5>
     <!-- Form for user input -->
     <form action="./order.php" method="post" target="results">
       <!-- Multiple choice - picking a meat type -->
@@ -63,7 +77,7 @@
         <option value="medium">Medium</option>
         <option value="large">Large</option>
       </select>
-      <h4>Choose Your Toppings:</h4>
+      <h5>Choose Your Toppings:</h5>
       <!-- Checkboxes for toppings -->
       <label for="tabbouleh">Tabbouleh</label>
       <input type="checkbox" id="tabbouleh" name="tabbouleh"><br><br>
@@ -77,7 +91,7 @@
       <input type="checkbox" id="pickles" name="pickles"><br><br>
       <label for="onions">Onions</label>
       <input type="checkbox" id="onions" name="onions"><br><br>
-      <h4>Choose Your Sides</h4>
+      <h5>Choose Your Sides</h5>
       <!-- Number input for garlic potato side -->
       <label for="potatoes">Garlic potatoes</label>
       <input type="number" id="potatoes" value="0" step="1" min="0" max="4" name="potatoes"><br><br>
